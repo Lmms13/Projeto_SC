80cfa945a766c9671bca02de9010b596d66ce3b1
pedropinto12345 <pedromrp1@gmail.com>
2023-03-12 17:00:55 +0000

    Added messages array to User

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,14 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class User {
 	private String userId;
 	private String password;
 	private int balance;
+	private HashMap<String, ArrayList<String>> messages;
 
 	public User(String id, String password) {
 		this.userId = id;
 		this.password = password;
 		this.balance = 200;
+		this.messages = new HashMap<String, ArrayList<String>>();
 	}
 
 	public String getId() {
@@ -18,21 +23,36 @@ public class User {
 	public String getPassword() {
 		return this.password;
 	}
-	
+
 	public int getBalance() {
 		return this.balance;
 	}
-	
+
 	public String toString() {
 		return this.userId + ":" + this.password;
 	}
-	
-	
+
 	public void addBalance(int n) {
 		this.balance += n;
 	}
-	
+
 	public void subtractBalance(int n) {
 		this.balance -= n;
+	}
+
+	public void addMessage(String sender, String message) {
+		if (messages.containsKey(sender)) {
+			messages.get(sender).add(message);
+		} else {
+			ArrayList<String> messageArray = new ArrayList<String>();
+			messageArray.add(message);
+			messages.put(sender, messageArray);
+		}
+	}
+
+	public HashMap<String, ArrayList<String>> getMessages() {
+		HashMap<String, ArrayList<String>> allMessages = messages;
+		messages = new HashMap<String, ArrayList<String>>();
+		return allMessages;
 	}
 }
